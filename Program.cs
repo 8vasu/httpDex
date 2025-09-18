a4c9cd5a610dd15e98b6e344022b705ac52ece6c
Soumendra Ganguly <soumendraganguly@gmail.com>
2025-09-18 06:59:26 +0200

    Initial commit.
    Signed-off-by: Soumendra Ganguly <soumendraganguly@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<PokemonContext>(options => options.UseSqlite("Data Source=pokemon.db"));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PokemonContext>();
+    db.Database.EnsureCreated();
+    
+    if (!db.Pokemon.Any())
+    {
+        var client = new HttpClient();
+        var response = await client.GetFromJsonAsync<PokemonList>("https://pokeapi.co/api/v2/pokemon?limit=151");
+        
+        foreach (var item in response.Results)
+        {
+            var pokemon = await client.GetFromJsonAsync<PokemonData>(item.Url);
+            db.Pokemon.Add(new Pokemon 
+            { 
+                Name = pokemon.Name, 
+                Height = pokemon.Height, 
+                Weight = pokemon.Weight 
+            });
+        }
+        db.SaveChanges();
+    }
+}
+
+app.MapGet("/pokemon", (PokemonContext db) => db.Pokemon.ToList());
+app.Run();
+
+public class Pokemon
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public int Height { get; set; }
+    public int Weight { get; set; }
+}
+
+public class PokemonContext : DbContext
+{
+    public PokemonContext(DbContextOptions<PokemonContext> options) : base(options) { }
+    public DbSet<Pokemon> Pokemon { get; set; }
+}
+
+public class PokemonList
+{
+    public List<PokemonItem> Results { get; set; }
+}
+
+public class PokemonItem
+{
+    public string Url { get; set; }
+}
+
+public class PokemonData
+{
+    public string Name { get; set; }
+    public int Height { get; set; }
+    public int Weight { get; set; }
+}
